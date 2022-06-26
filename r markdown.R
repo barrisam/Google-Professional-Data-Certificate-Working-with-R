f300825ecd2453b2d204f22cfc37b4c3287ab009
Barri <40701426+barrisam@users.noreply.github.com>
2022-06-26 01:19:14 +0100

    Create r markdown.R

---
+++ r markdown.R
@@ -0,0 +1,22 @@
+---
+title: "r markdown"
+author: "barri"
+date: '2022-06-03'
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## R Markdown
+
+This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+
+### embedding a link
+Another way to add a link is by doing this:\
+click here [click this link](http://rmarkdown.rstudio.com).
+The link is embedded in the text 'click this link'
+
+### Embedding an image
+![here is an embedded image](https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg)
